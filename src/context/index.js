9c163dbd36a2243b85cc5a370337d75a66943b9d
orun sunny <sunny.mukut@gmail.com>
2024-02-14 00:21:31 +0600

    added

--- src/context/index.js
+++ src/context/index.js
@@ -1,4 +1,5 @@
 import { createContext } from "react";
 const WeatherContext = createContext("");
 const FavouritContext = createContext("");
-export { FavouritContext, WeatherContext };
+const LocationContext = createContext("")
+export { FavouritContext, WeatherContext, LocationContext };
